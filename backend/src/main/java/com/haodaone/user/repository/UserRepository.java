c84bcce1b8de3b2a82a60364cae2d531f02b523e
Vikkash <brokzvicky@gmail.com>
2026-08-04 11:04:03 +0530

    haodaonesss

--- backend/src/main/java/com/haodaone/user/repository/UserRepository.java
+++ backend/src/main/java/com/haodaone/user/repository/UserRepository.java
@@ -2,12 +2,22 @@ package com.haodaone.user.repository;
 
 import com.haodaone.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsernameAndDeletedFalse(String username);
+    @Query("""
+        SELECT DISTINCT u
+        FROM User u
+        LEFT JOIN FETCH u.roles r
+        LEFT JOIN FETCH r.permissions
+        WHERE u.username = :username
+          AND u.deleted = false
+        """)
+    Optional<User> findByUsernameAndDeletedFalse(@Param("username") String username);
 
     Optional<User> findByEmailAndDeletedFalse(String email);
 
