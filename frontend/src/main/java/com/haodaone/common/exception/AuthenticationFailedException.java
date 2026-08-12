3f2b9ac7414c3080e6385bb64dbcfa55bb098c8a
brokzvicky <brokzvicky@gmail.com>
2026-08-12 17:16:00 +0530

    delete

--- frontend/src/main/java/com/haodaone/common/exception/AuthenticationFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.haodaone.common.exception;
-
-/** Thrown for authentication failures - wrong credentials, expired or malformed tokens. Maps to HTTP 401. */
-public class AuthenticationFailedException extends RuntimeException {
-    public AuthenticationFailedException(String message) {
-        super(message);
-    }
-}
--- frontend/src/main/java/com/haodaone/common/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.haodaone.common.exception;
-
-/** Thrown when a requested entity doesn't exist (or is soft-deleted). Maps to HTTP 404. */
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
