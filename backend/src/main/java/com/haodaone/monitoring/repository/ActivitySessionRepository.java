2c0a7b8afc2407fcc99e2ad3d1fa1ff3b7cd98c8
brokzvicky <brokzvicky@gmail.com>
2026-08-22 23:46:02 +0530

    pahse20

--- backend/src/main/java/com/haodaone/monitoring/repository/ActivitySessionRepository.java
+++ backend/src/main/java/com/haodaone/monitoring/repository/ActivitySessionRepository.java
@@ -41,8 +41,7 @@ where s.startTime >= :from
 and s.startTime < :to
 and (:employeeId is null or e.id = :employeeId)
 and (:employeeCode is null or e.employeeCode = :employeeCode)
-and (:employeeName is null or e.fullName like concat('%', :employeeName, '%'))
-and (:departmentId is null or e.department.id = :departmentId)
+and (:employeeName is null or lower(concat(e.firstName, ' ', e.lastName)) like lower(concat('%', :employeeName, '%')))and (:departmentId is null or e.department.id = :departmentId)
 and (:deviceId is null or d.id = :deviceId)
 and (:deviceName is null or d.deviceName like concat('%', :deviceName, '%'))
 order by s.startTime asc
